a415b7929bb4d0a51ba1a4737336a542979448e6
tiyan <tiyan@mamiteam.com>
2020-04-30 05:16:01 +0700

    Initial Commit

---
+++ FirstSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstSwiftUI
+//
+//  Created by Mamikos on 30/04/20.
+//  Copyright © 2020 Mamikos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
